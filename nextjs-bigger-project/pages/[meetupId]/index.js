46b403af4410e31bd1f7d931606efe9e9d73073b
curator69 <75662004+curator69@users.noreply.github.com>
2022-12-02 22:33:26 +0530

    added meetups page

---
+++ nextjs-bigger-project/pages/[meetupId]/index.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+const MeetupDetails = () => {
+  return (
+    <Fragment>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        alt="A First Meetup"
+      />
+      <h1>A First Meetup</h1>
+      <address>Some Street 5, Some City</address>
+      <p>The Meetup description</p>
+    </Fragment>
+  );
+};
+
+export default MeetupDetails;
